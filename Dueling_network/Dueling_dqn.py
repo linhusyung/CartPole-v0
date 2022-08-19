77566e6f83ec528e8ee29e1f507b4e762698a618
unknown <lin1620099471@gmail.com>
2022-08-19 21:17:09 +0800

    dueling

---
+++ Dueling_network/Dueling_dqn.py
@@ -0,0 +1,100 @@
+import gym
+import torch
+import numpy as np
+from network_torch import DQN_torch, Replay_Buffers
+import matplotlib.pyplot as plt
+
+
+class agent():
+    def __init__(self):
+        self.observation = env.reset()
+        self.model = DQN_torch(state_dim=len(self.observation))
+        self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+        self.epoch = 500
+        self.eps = 0.99
+        self.replay_buffers = Replay_Buffers()
+        self.gamma = 0.9
+        self.loss_fn = torch.nn.MSELoss()
+        self.optimizer = torch.optim.Adam(self.model.parameters(), lr=1e-3)
+
+    def np_to_tensor(self, np_data):
+        return torch.tensor(np_data, dtype=torch.float32)
+
+    def choose_action(self, out):
+        if np.random.random() < self.eps:
+            return np.random.randint(0, 2)
+        else:
+            return int(out.argmax().cpu().numpy())
+
+    def tarin(self, replay):
+        state_ = []
+        next_state_ = []
+        action_ = []
+        reward_ = []
+        done_ = []
+        for i in range(len(replay)):
+            state_.append(replay[i]['state'])
+            next_state_.append(replay[i]['next_state'])
+            action_.append(replay[i]['action'])
+            reward_.append(replay[i]['reward'])
+            done_.append(replay[i]['done'])
+        state_, next_state_, action_, reward_, done_ = \
+            tuple(state_), tuple(next_state_), self.np_to_tensor(action_).to(self.device), self.np_to_tensor(
+                reward_).to(self.device), self.np_to_tensor(done).to(self.device)
+
+        Q = self.model(self.np_to_tensor(state_)).to(self.device)
+        Q_next = self.model(self.np_to_tensor(next_state_)).to(self.device)
+        Q_target = reward_ + self.gamma * torch.max(Q_next, 1).values * (1 - done_)
+        Q_eval = Q.gather(1, action_.unsqueeze(1).type(torch.int64)).squeeze(1)
+
+        loss = self.loss_fn(Q_target, Q_eval)
+        self.optimizer.zero_grad()
+        loss.backward()
+        self.optimizer.step()
+    def save_model(self):
+        torch.save(self.model.state_dict(), 'model/model_params.pth')
+
+if __name__ == '__main__':
+    env = gym.make('CartPole-v0')
+    a = agent()
+    reward_list = []
+    b_list = []
+    for b in range(a.epoch):
+        b_list.append(b)
+        reward_sum = 0
+        observation = env.reset()
+        print('第', b, '次遊戲')
+        while True:
+            # env.render()
+            if b == a.epoch - 1:
+                env.render()
+            state = observation
+            obe = a.np_to_tensor(observation)
+            out = a.model(obe).to(a.device)
+            # print(out)
+            action = a.choose_action(out)
+            # print(action)
+            observation,r, done, info = env.step(action)
+
+            next_state = observation
+            x, v, theta, omega = next_state
+            r1 = (env.x_threshold - abs(x)) / env.x_threshold - 0.8
+            r2 = (env.theta_threshold_radians - abs(theta)) / env.theta_threshold_radians - 0.5
+            reward = r1 + r2
+            replay = a.replay_buffers.write_Buffers(state, next_state, reward, action, done)
+
+            reward_sum += r
+            if replay is not None:
+                a.tarin(replay)
+            if done:
+                if a.eps > 0.1:
+                    # a.eps -= a.eps*0.009
+                    a.eps *=0.9
+                print('eps', a.eps)
+                reward_list.append(reward_sum)
+                print(reward_sum)
+                break
+    a.save_model()
+    plt.plot(b_list, reward_list)
+    plt.show()
+    print(max(reward_list))
---
+++ Dueling_network/text.py
@@ -0,0 +1,27 @@
+import gym
+import torch
+from network_torch import DQN_torch
+import numpy as np
+class agent():
+    def __init__(self):
+        self.observation = env.reset()
+        self.model=self.model = DQN_torch(state_dim=len(self.observation))
+        self.epoch=20
+        self.PATH='model/model_params.pth'
+    def choose_action(self, out):
+        return int(out.argmax().cpu().numpy())
+    def np_to_tensor(self, np_data):
+        return torch.tensor(np_data, dtype=torch.float32)
+if __name__ == '__main__':
+    env=gym.make('CartPole-v0')
+    a=agent()
+    for b in range(a.epoch):
+        observation = env.reset()
+        print('第', b, '次遊戲')
+        while True:
+            env.render()
+            a.model.load_state_dict(torch.load(a.PATH))
+            state=a.np_to_tensor(observation)
+            out=a.model(state)
+            action=a.choose_action(out)
+            observation, r, done, info = env.step(action)
