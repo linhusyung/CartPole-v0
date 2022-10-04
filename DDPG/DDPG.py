74b6c40ee3137849f3d8ea2bc71c36d4f8ae83a6
lin hsu yung <lin1620099471@gmail.com>
2022-10-04 22:56:51 +0800

    “DDPG_fight”

---
+++ DDPG/DDPG.py
@@ -0,0 +1,86 @@
+# 角速度，角速度，水平速度，垂直速度，关节位置和关节角速度，腿是否与地面的接触以及10个激光雷达测距仪的测量值
+# 獎勵前進，總共 300+ 點到遠端。如果機器人摔倒，
+# 它得到-100。應用電機扭矩花費少量積分，更優代理
+# 會得到更好的分數。
+# 要解決這個遊戲，你需要在 1600 個時間步中獲得 300 分。
+# 要解決硬核版本，您需要 2000 個時間步長中的 300 個點。
+import gym
+import torch
+import numpy as np
+
+from DDPG_network import *
+
+
+class agent():
+    def __init__(self):
+        self.epoch = 1
+        self.state_dim = len(env.reset())
+
+        self.Actor = Actor(self.state_dim)
+        self.target_Actor = Actor(self.state_dim)
+
+        self.Critic = Critic(self.state_dim, 4)
+        self.target_Critic = Critic(self.state_dim, 4)
+
+        self.replay = Replay_Buffers()
+        self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+    def to_tensor(self, data):
+        return torch.tensor(data, dtype=torch.float32)
+
+    def np_to_tensor(self, data):
+        return torch.from_numpy(data).to(torch.float32)
+
+    def tuple_of_tensor_to_tensor(self, tuplt_of_tensor):
+        return torch.unsqueeze(torch.stack(tuplt_of_tensor, dim=0).squeeze(), 1)
+
+    def tensor_to_np(self, data):
+        return data.cpu().detach().numpy()
+
+    def choose_action(self, action):
+        noise = torch.tensor(np.random.normal(loc=0.0, scale=0.1), dtype=torch.float32).to(self.device)
+        return torch.clamp(action + noise, -1, 1)
+
+    def read_replay(self, replay):
+        state = []
+        state_next = []
+        reward = []
+        action = []
+        done = []
+        for i in range(len(replay)):
+            state.append(replay[i]['state'])
+            state_next.append(replay[i]['next_state'])
+            reward.append(replay[i]['reward'])
+            action.append(replay[i]['action'])
+            done.append(replay[i]['done'])
+        return self.tuple_of_tensor_to_tensor(state), self.tuple_of_tensor_to_tensor(
+            state_next), reward, self.tuple_of_tensor_to_tensor(action).cpu(), done
+
+    def train(self, replay):
+        state, state_next, reward, action, done = self.read_replay(replay)
+        print(self.target_Actor(state).to(self.device))
+        print(self.target_Critic(state, action).to(self.device))
+        # updata Critic
+
+
+
+if __name__ == '__main__':
+    env = gym.make('BipedalWalker-v3')
+    a = agent()
+
+    for i in range(a.epoch):
+        observation = env.reset()
+        print('第', i, '次遊戲')
+        while True:
+            # env.render()
+            state_now = a.np_to_tensor(observation)
+            action = a.choose_action(a.Actor(state_now).to(a.device))
+
+            observation, reward, done, info = env.step(a.tensor_to_np(action))
+
+            replay_sample = a.replay.write_Buffers(state_now, a.np_to_tensor(observation), reward, action, done)
+            if replay_sample is not None:
+                a.train(replay_sample)
+                break
+            if done:
+                break
---
+++ DDPG/DDPG_network.py
@@ -0,0 +1,67 @@
+import torch
+from torch import nn
+import torch.nn.functional as F
+from collections import deque
+from random import sample
+
+class Actor(nn.Module):
+    def __init__(self, state_dim):
+        super(Actor, self).__init__()
+        self.actor_fc1 = nn.Linear(state_dim, 64)
+        self.actor_fc1.weight.data.normal_(0, 0.1)
+        self.actor_fc1_ = nn.LayerNorm(64)
+
+        self.actor_fc2 = nn.Linear(64, 32)
+        self.actor_fc2.weight.data.normal_(0, 0.1)
+        self.actor_fc2_ = nn.LayerNorm(32)
+
+        self.actor_fc3 = nn.Linear(32, 4)
+        self.actor_fc3.weight.data.normal_(0, 0.1)
+
+    def forward(self, x):
+        x = F.relu(self.actor_fc1(x))
+        x = self.actor_fc1_(x)
+
+        x = F.relu(self.actor_fc2(x))
+        x = self.actor_fc2_(x)
+
+        x = torch.tanh(self.actor_fc3(x))
+        return x
+
+
+class Critic(nn.Module):
+    def __init__(self, state_dim, action_dim):
+        super(Critic, self).__init__()
+        self.Critic_h1 = nn.Linear(state_dim, 64)
+        self.Critic_h1.weight.data.normal_(0, 0.1)
+        self.Critic_h1_ = nn.LayerNorm(64)
+
+        self.Critic_action = nn.Linear(action_dim, 32)
+        self.Critic_action.weight.data.normal_(0, 0.1)
+
+        self.Critic_g1 = nn.Linear(64 + 32, 32)
+        self.Critic_g1.weight.data.normal_(0, 0.1)
+        self.Critic_g1_ = nn.LayerNorm(32)
+
+        self.Critic_g2 = nn.Linear(32, 1)
+        self.Critic_g2.weight.data.normal_(0, 0.1)
+
+    def forward(self, state, action):
+        x = self.Critic_h1_(F.relu(self.Critic_h1(state)))
+        action = F.relu(self.Critic_action(action))
+        goal = torch.cat([x, action], dim=-1)
+        x = self.Critic_g1_(F.relu(self.Critic_g1(goal)))
+        x = self.Critic_g2(x)
+        return x
+
+class Replay_Buffers():
+    def __init__(self):
+        self.buffer_size = 5000
+        self.buffer = deque([], maxlen=self.buffer_size)
+        self.batch = 20
+
+    def write_Buffers(self, state, next_state, reward, action, done):
+        once = {'state': state, 'next_state': next_state, 'reward': reward, 'action': action, 'done': done, }
+        self.buffer.append(once)
+        if len(self.buffer) >self.batch:
+            return sample(self.buffer, self.batch)
